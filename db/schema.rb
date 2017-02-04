950e29160bee5244b59866f6d00e00f92a349160
Yasuhito Maruichi <maruichi-yasuhito@t3-lab.com>
2017-02-04 09:36:56 +0000

    add age_column

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202012338) do
+ActiveRecord::Schema.define(version: 20170204074227) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
